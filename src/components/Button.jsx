a9ade9e8addf35536834b7a510edd67945afdc29
Jithesh09 <jithkrip@gmail.com>
2025-03-07 01:02:54 +0530

    subcribe section completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({label,iconURL,backgroundColor,textColor,borderColor}) => {
+const Button = ({label,iconURL,backgroundColor,textColor,borderColor,fullWidth}) => {
   return (
-    <button className= "flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red  text-white border-coral-red rounded-full">
+    <button className= {`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red  text-white border-coral-red rounded-full ${fullWidth && 'w-full'}`}>
       {label}
       {iconURL && <img src={iconURL}
       alt='arrow-right icon'
